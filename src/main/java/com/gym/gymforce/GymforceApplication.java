be022c47d4b30aabe63f499bf1deeaea356d57b1
rafaqueiroz94 <rafaqueiroz94@outlook.com>
2021-06-14 20:39:12 -0300

    first commit

---
+++ src/main/java/com/gym/gymforce/GymforceApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gymforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymforceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/gymforce/GymforceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.gymforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
